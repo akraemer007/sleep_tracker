cd2e338a5579ed01056f28202f9eaf762ef3916d
akraemer007 <kidman007@gmail.com>
2017-11-30 17:02:56 -0600

    Add files via upload

---
+++ boosting.py
@@ -0,0 +1,88 @@
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.base import clone
+
+
+class AdaBoostBinaryClassifier(object):
+    '''
+    INPUT:
+    - n_estimator (int)
+      * The number of estimators to use in boosting
+      * Default: 50
+
+    - learning_rate (float)
+      * Determines how fast the error would shrink
+      * Lower learning rate means more accurate decision boundary,
+        but slower to converge
+      * Default: 1
+    '''
+
+    def __init__(self,
+                 n_estimators=50,
+                 learning_rate=1):
+
+        self.base_estimator = DecisionTreeClassifier(max_depth=1)
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+
+        # Will be filled-in in the fit() step
+        self.estimators_ = []
+        self.estimator_weight_ = np.zeros(self.n_estimator, dtype=np.float)
+
+    def fit(self, x, y):
+        '''
+        INPUT:
+        - x: 2d numpy array, feature matrix
+        - y: numpy array, labels
+
+        Build the estimators for the AdaBoost estimator.
+        '''
+        for i in range(self.n_estimators):
+            self._boost(x, y, 1 / i)
+        pass  ### YOUR CODE HERE ###
+
+
+    def _boost(self, x, y, sample_weight):
+        '''
+        INPUT:
+        - x: 2d numpy array, feature matrix
+        - y: numpy array, labels
+        - sample_weight: numpy array
+
+        OUTPUT:
+        - estimator: DecisionTreeClassifier
+        - sample_weight: numpy array (updated weights)
+        - estimator_weight: float (weight of estimator)
+
+        Go through one iteration of the AdaBoost algorithm. Build one estimator.
+        '''
+
+        estimator = clone(self.base_estimator)
+        estimator.fit(X, y, sample_weight=sample_weight)
+        predict_y = estimator.predict(X)
+        estimator_error = sample_weight.dot(y - predict_y) / sample_weight
+
+
+    def predict(self, x):
+        '''
+        INPUT:
+        - x: 2d numpy array, feature matrix
+
+        OUTPUT:
+        - labels: numpy array of predictions (0 or 1)
+        '''
+
+        pass  ### YOUR CODE HERE ###
+
+
+    def score(self, x, y):
+        '''
+        INPUT:
+        - x: 2d numpy array, feature matrix
+        - y: numpy array, labels
+
+        OUTPUT:
+        - score: float (accuracy score between 0 and 1)
+        '''
+
+        pass  ### YOUR CODE HERE ###
